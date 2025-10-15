fb64b0830e35036d758b29306c3c3f10c223984c
YunHak Lee <lyh990408@gmail.com>
2025-10-15 14:47:42 +0900

    워크스페이스 추가, App 모듈 추가

---
+++ Workspace.swift
@@ -0,0 +1,16 @@
+//
+//  Workspace.swift
+//  Config
+//
+//  Created by YunhakLee on 10/1/25.
+//
+
+import Foundation
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "WSS-iOS-V2",
+    projects: [
+        "Projects/App"
+    ]
+)
