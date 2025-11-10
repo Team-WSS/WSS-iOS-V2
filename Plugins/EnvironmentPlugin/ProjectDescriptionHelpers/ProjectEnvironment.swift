905cc0d661a6a3f6ff5e11920a008c8c1f9911c7
Seoyeon Choi <lala1470@konkuk.ac.kr>
2025-11-10 18:13:27 +0900

    [Setting] #9 - EnvironmentPlugin 추가
    앱 전반에 사용되는 변수를 관리하는 EnvironmentPlugin을 추가하였습니다.

---
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectEnvironment.swift
+//  ConfigurationPlugin
+//
+//  Created by Seoyeon Choi on 11/10/25.
+//
+
+import ProjectDescription
+
+public struct ProjectEnvironment {
+    public let appName: String
+    public let targetName: String
+    public let targetTestName: String
+    public let organizationName: String
+    public let deploymentTarget: DeploymentTargets
+    public let destination: ProjectDescription.Destinations
+    public let baseSetting: SettingsDictionary
+}
+
+public let env = ProjectEnvironment(
+    appName: "Websoso",
+    targetName: "WSS-iOS",
+    targetTestName: "WSS-iOSTests",
+    organizationName: "io.tuist.WSS",
+    deploymentTarget: .iOS("15.0"),
+    destination: .iOS,
+    baseSetting: [:]
+)
+
