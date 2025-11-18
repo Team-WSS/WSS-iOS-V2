5651765dd688dfec4b091e98e2eeb4ac3e9c513c
Seoyeon Choi <lala1470@konkuk.ac.kr>
2025-11-18 17:29:52 +0900

    [Fix] #9 - 모듈 경로 관련 함수 파라미터에 String 대신 ModuleType 열거형이 선언되도록 수정

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -17,96 +17,95 @@ public extension TargetDependency {
 
 public extension TargetDependency.Feature {
     static let HomeFeature = TargetDependency.project(
-        target: ModuleType.Feature.HomeFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModuleType.Feature.HomeFeature.rawValue)
+        target: ModuleType.Feature.home.targetName(type: .sources),
+        path: .relativeToFeature(.home)
     )
     
     static let FeedFeature = TargetDependency.project(
-        target: ModuleType.Feature.FeedFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModuleType.Feature.FeedFeature.rawValue)
+        target: ModuleType.Feature.feed.targetName(type: .sources),
+        path: .relativeToFeature(.feed)
     )
 }
 
 public extension TargetDependency.Domain {
     static let RecommendationDomain = TargetDependency.project(
-        target: ModuleType.Domain.RecommendationDomain.targetName(type: .sources),
-        path: .relativeToDomain(ModuleType.Domain.RecommendationDomain.rawValue)
+        target: ModuleType.Domain.recommendation.targetName(type: .sources),
+        path: .relativeToDomain(.recommendation)
     )
     
     static let RecommendationDomainDemo = TargetDependency.project(
-        target: ModuleType.Domain.RecommendationDomain.targetName(type: .demo),
-        path: .relativeToDomain(ModuleType.Domain.RecommendationDomain.rawValue)
+        target: ModuleType.Domain.recommendation.targetName(type: .demo),
+        path: .relativeToDomain(.recommendation)
     )
     
     static let RecommendationDomainTests = TargetDependency.project(
-        target: ModuleType.Domain.RecommendationDomain.targetName(type: .tests),
-        path: .relativeToDomain(ModuleType.Domain.RecommendationDomain.rawValue)
+        target: ModuleType.Domain.recommendation.targetName(type: .tests),
+        path: .relativeToDomain(.recommendation)
     )
 }
 
 public extension TargetDependency.Data {
     static let RecommendationData = TargetDependency.project(
-        target: ModuleType.Data.RecommendationData.targetName(type: .sources),
-        path: .relativeToDomain(ModuleType.Data.RecommendationData.rawValue)
+        target: ModuleType.Data.recommendation.targetName(type: .sources),
+        path: .relativeToDomain(.recommendation)
     )
     
     static let RecommendationDataDemo = TargetDependency.project(
-        target: ModuleType.Data.RecommendationData.targetName(type: .demo),
-        path: .relativeToDomain(ModuleType.Data.RecommendationData.rawValue)
+        target: ModuleType.Data.recommendation.targetName(type: .demo),
+        path: .relativeToDomain(.recommendation)
     )
     
     static let RecommendationDataTests = TargetDependency.project(
-        target: ModuleType.Data.RecommendationData.targetName(type: .tests),
-        path: .relativeToDomain(ModuleType.Data.RecommendationData.rawValue)
+        target: ModuleType.Data.recommendation.targetName(type: .tests),
+        path: .relativeToDomain(.recommendation)
     )
 }
 
 public extension TargetDependency.Core {
     static let Keychain = TargetDependency.project(
-        target: ModuleType.Core.Keychain.targetName(type: .sources),
-        path: .relativeToCore(ModuleType.Core.Keychain.rawValue)
+        target: ModuleType.Core.keyChain.targetName(type: .sources),
+        path: .relativeToCore(.keyChain)
     )
     
     static let KeychainDemo = TargetDependency.project(
-        target: ModuleType.Core.Keychain.targetName(type: .demo),
-        path: .relativeToCore(ModuleType.Core.Keychain.rawValue)
+        target: ModuleType.Core.keyChain.targetName(type: .demo),
+        path: .relativeToCore(.keyChain)
     )
     
     static let Logger = TargetDependency.project(
-        target: ModuleType.Core.Logger.targetName(type: .sources),
-        path: .relativeToCore(ModuleType.Core.Logger.rawValue)
+        target: ModuleType.Core.logger.targetName(type: .sources),
+        path: .relativeToCore(.logger)
     )
     
     static let LoggerDemo = TargetDependency.project(
-        target: ModuleType.Core.Logger.targetName(type: .demo),
-        path: .relativeToCore(ModuleType.Core.Logger.rawValue)
+        target: ModuleType.Core.logger.targetName(type: .demo),
+        path: .relativeToCore(.logger)
     )
     
     static let Networking = TargetDependency.project(
-        target: ModuleType.Core.Networking.targetName(type: .sources),
-        path: .relativeToCore(ModuleType.Core.Networking.rawValue)
+        target: ModuleType.Core.networking.targetName(type: .sources),
+        path: .relativeToCore(.networking)
     )
     
     static let NetworkingDemo = TargetDependency.project(
-        target: ModuleType.Core.Networking.targetName(type: .demo),
-        path: .relativeToCore(ModuleType.Core.Networking.rawValue)
+        target: ModuleType.Core.networking.targetName(type: .demo),
+        path: .relativeToCore(.networking)
     )
     
     static let NetworkingTests = TargetDependency.project(
-        target: ModuleType.Core.Networking.targetName(type: .tests),
-        path: .relativeToCore(ModuleType.Core.Networking.rawValue)
+        target: ModuleType.Core.networking.targetName(type: .tests),
+        path: .relativeToCore(.networking)
     )
 }
 
 public extension TargetDependency.UI {
     static let DesignSystem = TargetDependency.project(
-        target: ModuleType.UI.DesignSystem.targetName(type: .sources),
-        path: .relativeToCore(ModuleType.UI.DesignSystem.rawValue)
+        target: ModuleType.UI.designSystem.targetName(type: .sources),
+        path: .relativeToUI(.designSystem)
     )
     
     static let WSSComponent = TargetDependency.project(
-        target: ModuleType.UI.WSSComponent.targetName(type: .sources),
-        path: .relativeToCore(ModuleType.UI.WSSComponent.rawValue)
+        target: ModuleType.UI.wssComponent.targetName(type: .sources),
+        path: .relativeToUI(.wssComponent)
     )
-    
 }
--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/ProjectDescriptionPath+.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/ProjectDescriptionPath+.swift
@@ -11,19 +11,19 @@ public extension ProjectDescription.Path {
     static func relativeToSections(_ path: String) -> Self {
         return .relativeToRoot("Projects/\(path)")
     }
-    static func relativeToFeature(_ path: String) -> Self {
+    static func relativeToFeature(_ path: ModuleType.Feature) -> Self {
         return .relativeToRoot("Projects/Feature/\(path)")
     }
-    static func relativeToDomain(_ path: String) -> Self {
+    static func relativeToDomain(_ path: ModuleType.Domain) -> Self {
         return .relativeToRoot("Projects/Domain/\(path)")
     }
-    static func relativeToData(_ path: String) -> Self {
+    static func relativeToData(_ path: ModuleType.Data) -> Self {
         return .relativeToRoot("Projects/Data/\(path)")
     }
-    static func relativeToCore(_ path: String) -> Self {
+    static func relativeToCore(_ path: ModuleType.Core) -> Self {
         return .relativeToRoot("Projects/Core/\(path)")
     }
-    static func relativeToUI(_ path: String) -> Self {
+    static func relativeToUI(_ path: ModuleType.UI) -> Self {
         return .relativeToRoot("Projects/UI/\(path)")
     }
     static var app: Self {
