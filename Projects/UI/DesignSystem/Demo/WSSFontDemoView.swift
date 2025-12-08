c41e1244230b0c3e7538dbd1945751b7a67b0ad6
Seoyeon Choi <lala1470@konkuk.ac.kr>
2025-12-08 18:36:16 +0900

    [Fix] #15 - Color Asset 폴더링 수정 | Image Asset 내 이미지 추가

--- Projects/UI/DesignSystem/Demo/WSSFontDemoView.swift
+++ Projects/UI/DesignSystem/Demo/WSSFontDemoView.swift
@@ -14,15 +14,15 @@ struct WSSFontDemoView: View {
         VStack {
             Text("제목입니다 제목이라구요 제목이야 이게 제목이라고")
                 .applyWSSFont(.headline1)
-                .border(DesignSystemAsset.PrimaryColors.wssPrimary100.swiftUIColor)
+                .border(DesignSystemAsset.Colors.wssPrimary100.swiftUIColor)
            
             Text("마법학교 마법사로 살아가는 법마법학교 마법사로 살아가는 법마법학교 마법사로 살아가는 법")
                 .applyWSSFont(.title3)
-                .border(DesignSystemAsset.PrimaryColors.wssPrimary100.swiftUIColor)
+                .border(DesignSystemAsset.Colors.wssPrimary100.swiftUIColor)
             
             Text("대학원생이 환생에서 대학원생이 됨. 주인공 완전 갓갓! 일단 작가가 세계관이나 마법에 대해서 진지하게 생각해보고설정을 짠게 느껴져서 좋아요. 요즘 하도 라이트하고 가짜 마법물이 많아서 ㅠ 찐 성장+마법물!!글고 일단 작가님 필력이 무난하게 잘뽑으시고 연재도 빠르")
                 .applyWSSFont(.body2)
-                .border(DesignSystemAsset.PrimaryColors.wssPrimary100.swiftUIColor)
+                .border(DesignSystemAsset.Colors.wssPrimary100.swiftUIColor)
         }
         .padding(40)
     }
