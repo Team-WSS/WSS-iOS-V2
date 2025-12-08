acfe8e60c1dd62f3334afe1c3e63e2bf977c592b
Seoyeon Choi <lala1470@konkuk.ac.kr>
2025-12-08 17:51:31 +0900

    [Feat] #15 - letterSpacing, verticalPadding값 고려하여 폰트 확장 함수 적용

--- Projects/UI/DesignSystem/Sources/View+applyWSSFont.swift
+++ Projects/UI/DesignSystem/Sources/View+applyWSSFont.swift
@@ -10,19 +10,13 @@ import UIKit
 import SwiftUI
 
 extension View {
-    /// 뷰에 applyWSSFont 메소드를 활용하여 폰트를 지정합니다.
     public func applyWSSFont(_ style: WSSFontStyle) -> some View {
-        let swiftUIFont = style.fontName.swiftUIFont(size: style.fontSize)
-        
-        let calculatedLineHeight = style.fontSize * (style.lineHeight / 100)
-        let kerning = (style.letterSpacing / 100) * style.fontSize
-        let lineSpacing = max(0, calculatedLineHeight - style.lineHeight)
-        let verticalPadding = max(0, lineSpacing / 2)
-        
-        return self
-            .font(swiftUIFont)
-            .kerning(kerning)
-            .lineSpacing(lineSpacing)
-            .padding(.vertical, verticalPadding)
+        modifier(
+            WSSFontViewModifier(
+                uiFont: style.uiFontGuide(),
+                lineHeight: style.defaultFontSize * style.lineHeight,
+                letterSpacing: style.letterSpacing
+            )
+        )
     }
 }
