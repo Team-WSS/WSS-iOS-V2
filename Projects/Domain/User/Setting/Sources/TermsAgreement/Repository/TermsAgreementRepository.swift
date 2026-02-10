ddc5aad7499f37be9885f6bbe714c01d1af9fbd6
YunHak Lee <lyh990408@gmail.com>
2026-02-10 17:00:34 +0900

    [Feat] #29 - 약관동의 관련 UseCase / Repository 구현

---
+++ Projects/Domain/User/Setting/Sources/TermsAgreement/Repository/TermsAgreementRepository.swift
@@ -0,0 +1,15 @@
+//
+//  TermsAgreementRepository.swift
+//  SettingDomain
+//
+//  Created by YunhakLee on 2/10/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+
+// TermsAgreementRepository.swift
+
+public protocol TermsAgreementRepository {
+    func loadTermsAgreementDraft() async throws(RepositoryError) -> TermsAgreementDraft
+    func saveTermsAgreementDraft(_ draft: TermsAgreementDraft) async throws(RepositoryError)
+}
