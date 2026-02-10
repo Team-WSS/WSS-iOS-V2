c5b85f433aa716c97ea81240da2053b93f0e60ab
YunHak Lee <lyh990408@gmail.com>
2026-02-10 18:23:27 +0900

    [Test] #29 - TermsAgreement TestCode 작성

---
+++ Projects/Domain/User/Setting/Tests/TermsAgreement/UseCase/LoadTermsAgreementDraftUseCaseTests.swift
@@ -0,0 +1,46 @@
+//
+//  LoadTermsAgreementDraftUseCaseTests.swift
+//  SettingDomain
+//
+//  Created by YunhakLee on 2/10/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+
+import Testing
+@testable import SettingDomain
+
+@Suite("LoadTermsAgreementDraftUseCase")
+struct LoadTermsAgreementDraftUseCaseTests {
+
+    @Test("레포지토리에서 Draft를 불러와 그대로 반환한다")
+    func returnsDraftFromRepository() async throws {
+        let repo = MockTermsAgreementRepository()
+
+        var expected = TermsAgreementDraft()
+        let any = TermsType.allCases.first!
+        expected.setAgreed(true, for: any)
+        repo.loadResult = .success(expected)
+
+        let sut = DefaultLoadTermsAgreementDraftUseCase(repository: repo)
+
+        let draft = try await sut.execute()
+
+        #expect(repo.loadCallCount == 1)
+        #expect(draft == expected)
+    }
+
+    @Test("레포지토리에서 에러가 발생하면 그대로 전달한다")
+    func propagatesRepositoryError() async {
+        let repo = MockTermsAgreementRepository()
+        repo.loadResult = .failure(.networkUnavailable)
+
+        let sut = DefaultLoadTermsAgreementDraftUseCase(repository: repo)
+
+        await #expect(throws: RepositoryError.networkUnavailable) {
+            _ = try await sut.execute()
+        }
+
+        #expect(repo.loadCallCount == 1)
+    }
+}
---
+++ Projects/Domain/User/Setting/Tests/TermsAgreement/UseCase/SaveTermsAgreementDraftUseCaseTests.swift
@@ -0,0 +1,50 @@
+//
+//  SaveTermsAgreementDraftUseCaseTests.swift
+//  SettingDomain
+//
+//  Created by YunhakLee on 2/10/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+
+import Testing
+@testable import SettingDomain
+
+@Suite("SaveTermsAgreementDraftUseCase")
+struct SaveTermsAgreementDraftUseCaseTests {
+
+    @Test("Draft를 저장 요청하면 레포지토리에 그대로 전달한다")
+    func savesDraftToRepository() async throws
+    {
+        let repo = MockTermsAgreementRepository()
+        let sut = DefaultSaveTermsAgreementDraftUseCase(repository: repo)
+
+        var draft = TermsAgreementDraft()
+        
+        for t in TermsType.allCases where t.isRequired {
+            draft.setAgreed(true, for: t)
+        }
+
+        try await sut.execute(draft: draft)
+
+        #expect(repo.saveCallCount == 1)
+        #expect(repo.savedDrafts == [draft])
+    }
+
+    @Test("레포지토리에서 에러가 발생하면 그대로 전달한다")
+    func propagatesRepositoryError() async {
+        let repo = MockTermsAgreementRepository()
+        repo.saveResult = .failure(.authenticationRequired)
+
+        let sut = DefaultSaveTermsAgreementDraftUseCase(repository: repo)
+
+        let draft = TermsAgreementDraft()
+
+        await #expect(throws: RepositoryError.authenticationRequired) {
+            try await sut.execute(draft: draft)
+        }
+
+        #expect(repo.saveCallCount == 1)
+        #expect(repo.savedDrafts == [draft])
+    }
+}
