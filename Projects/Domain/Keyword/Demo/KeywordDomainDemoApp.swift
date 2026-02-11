ab012b36c8793778910bf5e1698efc501c889d44
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-02-11 23:13:29 +0900

    [Chore] #25 - Keyword Domain 내 DemoApp 파일 생성

---
+++ Projects/Domain/Keyword/Demo/KeywordDomainDemoApp.swift
@@ -0,0 +1,15 @@
+//
+//  KeywordDomainDemoApp.swift
+//  KeywordDomain
+//
+//  Created by Seoyeon Choi on 2/11/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+import SwiftUI
+
+struct KeywordDomainDemoApp: View {
+    var body: some View {
+        ContentView()
+    }
+}
