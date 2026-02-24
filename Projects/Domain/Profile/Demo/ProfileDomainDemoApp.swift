4368969918d6c79291e39ef93550d963e147f2c9
YunHak Lee <lyh990408@gmail.com>
2026-02-24 14:37:40 +0900

    [Add] #38 - Demo App 파일 추가

---
+++ Projects/Domain/Profile/Demo/ProfileDomainDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileDomainDemoApp.swift
+//  ProfileDomain
+//
+//  Created by YunhakLee on 2/24/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct ProfileDomainDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
