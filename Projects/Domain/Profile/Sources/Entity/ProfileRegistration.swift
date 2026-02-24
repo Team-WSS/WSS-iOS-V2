238de89b797da6bf236933524f86f21b9102a3d3
YunHak Lee <lyh990408@gmail.com>
2026-02-24 23:20:53 +0900

    [Feat] #38 - 프로필 등록 기능 구현

---
+++ Projects/Domain/Profile/Sources/Entity/ProfileRegistration.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileRegistration.swift
+//  ProfileDomain
+//
+//  Created by YunhakLee on 2/24/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+import BaseDomain
+
+public struct ProfileRegistration {
+    public let nickname: String
+    public let gender: Gender
+    public let birthYear: BirthYear
+    public let genrePreferences: [NovelGenre]
+}
