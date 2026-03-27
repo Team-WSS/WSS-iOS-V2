badd5f5ad8f4a29bc8d03dd193802f2f02c499a1
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-03-27 12:06:43 +0900

    [Chore] #69 - NovelData 모듈 생성 | NovelDomain 폴더명 수정

---
+++ Projects/Data/NovelData/Project.swift
@@ -0,0 +1,19 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by Seoyeon Choi on 3/27/26.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.createDataModule(
+    name: ModuleType.Data.novel.name,
+    targets: [.sources, .demo, .tests],
+    internalDependencies: [
+        .Core.Networking,
+        .Domain.NovelDomain
+    ]
+)
---
+++ Projects/Data/NovelData/Sources/Novel.swift
@@ -0,0 +1,8 @@
+//
+//  Novel.swift
+//  AppManifests
+//
+//  Created by Seoyeon Choi on 3/27/26.
+//
+
+import Foundation
