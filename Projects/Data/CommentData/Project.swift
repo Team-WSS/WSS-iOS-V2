0479cecf8c734a95a34f5e43e196f32905f41474
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-04-09 17:20:48 +0900

    [Feat] #64 - CommentData 모듈 생성 및 기존 CommentDomain 폴더명 수정

---
+++ Projects/Data/CommentData/Project.swift
@@ -0,0 +1,21 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by Seoyeon Choi on 4/9/26.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.createDataModule(
+    name: ModuleType.Data.comment.name,
+    targets: [.sources, .testing, .tests],
+    internalDependencies: [
+        .Core.Networking,
+        .Core.Logger,
+        .Domain.makeDependency(for: .base, type: .sources),
+        .Domain.makeDependency(for: .comment, type: .sources)
+    ]
+)
