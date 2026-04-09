d4cd736168ce255f13e19f6fff39c5f786a2a91e
YunHak Lee <lyh990408@gmail.com>
2026-04-09 22:48:44 +0900

    [Feat] #78 - AccessToken NetworkingClient에서 주입하도록 구현

--- Projects/Core/Networking/Sources/NetworkingClient.swift
+++ Projects/Core/Networking/Sources/NetworkingClient.swift
@@ -1,19 +1,23 @@
 import Foundation
+import Keychain
 
 public final class NetworkingClient: NetworkingRequestable {
     private let urlSession: URLSession
     private let logger: NetworkLogging?
+    private let tokenStore: TokenStore?
 
     public init(
         urlSession: URLSession = .shared,
-        logger: NetworkLogging? = nil
+        logger: NetworkLogging? = nil,
+        tokenStore: TokenStore? = nil
     ) {
         self.urlSession = urlSession
         self.logger = logger
+        self.tokenStore = tokenStore
     }
 
     public func request(_ endpoint: Endpoint) async throws -> Data {
-        let request = endpoint.urlRequest
+        let request = try authorizedRequest(for: endpoint)
         logger?.logRequest(request)
         
         let (data, response) = try await urlSession.data(for: request)
@@ -30,6 +34,21 @@ public final class NetworkingClient: NetworkingRequestable {
 }
 
 extension NetworkingClient {
+    private func authorizedRequest(for endpoint: Endpoint) throws -> URLRequest {
+        var request = endpoint.urlRequest
+
+        guard request.value(forHTTPHeaderField: "Authorization") == nil else {
+            return request
+        }
+
+        guard let accessToken = try tokenStore?.accessToken(), accessToken.isEmpty == false else {
+            return request
+        }
+
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+
     private func validateResponse(data: Data, response: URLResponse) throws {
         guard let http = response as? HTTPURLResponse else {
             throw NetworkingError.invalidURL
