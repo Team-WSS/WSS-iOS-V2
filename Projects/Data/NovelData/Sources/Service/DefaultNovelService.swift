eac163362d4ddfdee844e109460b9529009dd5b5
Guryss <lala1470@konkuk.ac.kr>
2026-04-14 23:53:11 +0900

    [Chore] #69 - DefaultNovelService 접근제어자 public 추가
    - DefaultNovelService 및 메서드 전체 public 접근제어자 추가

--- Projects/Data/NovelData/Sources/Service/DefaultNovelService.swift
+++ Projects/Data/NovelData/Sources/Service/DefaultNovelService.swift
@@ -9,50 +9,50 @@
 import Foundation
 import Networking
 
-struct DefaultNovelService: NovelService {
+public struct DefaultNovelService: NovelService {
     private let client: NetworkingRequestable
-    
-    init(client: NetworkingRequestable) {
+
+    public init(client: NetworkingRequestable) {
         self.client = client
     }
-    
-    func getUserLibraryNovels(userID: Int,
+
+    public func getUserLibraryNovels(userID: Int,
                               query: UserLibraryQuery) async throws -> UserLibraryNovelsResponse {
         let endpoint = NovelEndpoint.getUserLibraryNovels(userID: userID, query)
         return try await client.request(endpoint, decodeTo: UserLibraryNovelsResponse.self)
     }
-    
-    func getNovelBasicInfo(novelID: Int) async throws -> NovelBasicResponse {
+
+    public func getNovelBasicInfo(novelID: Int) async throws -> NovelBasicResponse {
         let endpoint = NovelEndpoint.getNovelBasicInfo(novelID: novelID)
         return try await client.request(endpoint, decodeTo: NovelBasicResponse.self)
     }
-    
-    func getNovelDetailInfo(novelID: Int) async throws -> NovelInfoResponse {
+
+    public func getNovelDetailInfo(novelID: Int) async throws -> NovelInfoResponse {
         let endpoint = NovelEndpoint.getNovelDetailInfo(novelID: novelID)
         return try await client.request(endpoint, decodeTo: NovelInfoResponse.self)
     }
-    
-    func getUserRegisteredNovelStats(userID: Int) async throws -> UserRegisteredNovelStatesResponse {
+
+    public func getUserRegisteredNovelStats(userID: Int) async throws -> UserRegisteredNovelStatesResponse {
         let endpoint = NovelEndpoint.getRegisteredNovelStats(userID: userID)
         return try await client.request(endpoint, decodeTo: UserRegisteredNovelStatesResponse.self)
     }
-    
-    func postNovelInterest(novelID: Int) async throws {
+
+    public func postNovelInterest(novelID: Int) async throws {
         let endpoint = NovelEndpoint.postNovelInterest(novelID: novelID)
         _ = try await client.request(endpoint)
     }
-    
-    func deleteNovelInterest(novelID: Int) async throws {
-        let endpoint = NovelEndpoint.postNovelInterest(novelID: novelID)
+
+    public func deleteNovelInterest(novelID: Int) async throws {
+        let endpoint = NovelEndpoint.deleteNovelInterest(novelID: novelID)
         _ = try await client.request(endpoint)
     }
-    
-    func getNormalSearchNovels(query: NormalSearchQuery) async throws -> SearchNovelsResponse {
+
+    public func getNormalSearchNovels(query: NormalSearchQuery) async throws -> SearchNovelsResponse {
         let endpoint = NovelEndpoint.getNormalSearchResult(query)
         return try await client.request(endpoint, decodeTo: SearchNovelsResponse.self)
     }
-    
-    func getDetailSearchNovels(query: DetailSearchQuery) async throws -> SearchNovelsResponse {
+
+    public func getDetailSearchNovels(query: DetailSearchQuery) async throws -> SearchNovelsResponse {
         let endpoint = NovelEndpoint.getDetailSearchResult(query)
         return try await client.request(endpoint, decodeTo: SearchNovelsResponse.self)
     }
