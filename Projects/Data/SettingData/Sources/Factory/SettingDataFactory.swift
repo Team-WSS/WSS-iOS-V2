8e88db431455d09a16fe08bb6cdf6290a6032351
YunHak Lee <lyh990408@gmail.com>
2026-04-20 14:40:46 +0900

    [Feat] #90 - Factory 구현

---
+++ Projects/Data/SettingData/Sources/Factory/SettingDataFactory.swift
@@ -0,0 +1,34 @@
+//
+//  SettingDataFactory.swift
+//  SettingData
+//
+//  Created by Codex on 5/20/26.
+//
+
+import BaseData
+import Networking
+import SettingDomain
+
+public enum SettingDataFactory {
+    public static func makeAppUpdateRepository(
+        client: NetworkingRequestable,
+        logger: DataLogger? = nil
+    ) -> AppUpdateRepository {
+        let service = DefaultSettingService(client: client)
+        return DefaultAppUpdateRepository(
+            settingService: service,
+            logger: logger
+        )
+    }
+
+    public static func makeTermsAgreementRepository(
+        client: NetworkingRequestable,
+        logger: DataLogger? = nil
+    ) -> TermsAgreementRepository {
+        let service = DefaultSettingService(client: client)
+        return DefaultTermsAgreementRepository(
+            settingService: service,
+            logger: logger
+        )
+    }
+}
