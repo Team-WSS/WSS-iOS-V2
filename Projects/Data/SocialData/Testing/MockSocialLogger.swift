fbaf51c61334ac5579f555b7c2091449f86a5978
onesunny2 <ippne996@gmail.com>
2026-05-01 19:22:32 +0900

    [Chore] #87 - MockSocialLogger 제거

--- Projects/Data/SocialData/Testing/MockSocialLogger.swift
+++ Projects/Data/SocialData/Testing/MockSocialLogger.swift
@@ -1,30 +1,8 @@
 //
-//  MockSocialLogger.swift
-//  SocialDataTesting
+//  SocialDataTesting.swift
+//  AppManifests
 //
 //  Created by YunhakLee on 4/23/26.
-//  Copyright © 2026 kr.websoso.app. All rights reserved.
 //
 
-@testable import SocialData
-
-public final class MockSocialLogger: SocialLogger {
-
-    public private(set) var logErrorCallCount = 0
-    public private(set) var loggedTypes: [SocialErrorType] = []
-    public private(set) var loggedActions: [SocialAction] = []
-    public private(set) var loggedErrors: [Error?] = []
-
-    public init() {}
-
-    public func logError(
-        type: SocialErrorType,
-        action: SocialAction,
-        error: Error?
-    ) {
-        logErrorCallCount += 1
-        loggedTypes.append(type)
-        loggedActions.append(action)
-        loggedErrors.append(error)
-    }
-}
+import Foundation
