58c050302d2354493f8de67a2313daebdbfb1fb2
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-05-01 13:49:56 +0900

    [Chore] #15 - DesignSystem 이미지 접근 축약 typealias 추가

--- Projects/UI/DesignSystem/Demo/WSSFontDemoView.swift
+++ Projects/UI/DesignSystem/Demo/WSSFontDemoView.swift
@@ -23,6 +23,12 @@ struct WSSFontDemoView: View {
             Text("대학원생이 환생에서 대학원생이 됨. 주인공 완전 갓갓! 일단 작가가 세계관이나 마법에 대해서 진지하게 생각해보고설정을 짠게 느껴져서 좋아요. 요즘 하도 라이트하고 가짜 마법물이 많아서 ㅠ 찐 성장+마법물!!글고 일단 작가님 필력이 무난하게 잘뽑으시고 연재도 빠르")
                 .applyWSSFont(.body2)
                 .border(DesignSystemAsset.Colors.wssPrimary100.swiftUIColor)
+            
+            Image(uiImage: WSSImage.imgLogoType.image)
+            Image(uiImage: WSSImage.icAnnouncement.image)
+            Image(uiImage: WSSImage.icTextHot.image)
+            WSSImage.icAnnouncement.swiftUIImage
+            WSSImage.imgLogoType.swiftUIImage
         }
         .padding(40)
     }
---
+++ Projects/UI/DesignSystem/Sources/Image/WSSImage.swift
@@ -0,0 +1,9 @@
+//
+//  WSSImage.swift
+//  DesignSystem
+//
+//  Created by Seoyeon Choi on 5/1/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+public typealias WSSImage = DesignSystemAsset.Images
