305a5648adbe23ec9b72228ea2d2ad3a5a4914f8
onesunny2 <ippne996@gmail.com>
2026-05-02 19:12:12 +0900

    [Fix] #71 - MockProfileLocalStorage AppStorage 채택으로 교체

--- Projects/Data/ProfileData/Testing/MockProfileLocalStorage.swift
+++ Projects/Data/ProfileData/Testing/MockProfileLocalStorage.swift
@@ -6,13 +6,38 @@
 //  Copyright © 2026 kr.websoso.app. All rights reserved.
 //
 
-@testable import ProfileData
+import BaseData
 
-public final class MockProfileLocalStorage: ProfileLocalStorage {
-    public var userID: Int?
-    public var nickname: String?
-    public var characterID: Int?
-    public var gender: String?
+public final class MockProfileLocalStorage: AppStorage {
+    private var storage: [String: Any] = [:]
 
     public init() {}
+
+    public func get<V>(_ key: StorageKey<V>) -> V? {
+        storage[key.rawValue] as? V
+    }
+
+    public func set<V>(_ key: StorageKey<V>, _ value: V?) {
+        storage[key.rawValue] = value
+    }
+
+    public var userID: Int? {
+        get { get(.userID) }
+        set { set(.userID, newValue) }
+    }
+
+    public var nickname: String? {
+        get { get(.nickname) }
+        set { set(.nickname, newValue) }
+    }
+
+    public var characterID: Int? {
+        get { get(.characterID) }
+        set { set(.characterID, newValue) }
+    }
+
+    public var gender: String? {
+        get { get(.gender) }
+        set { set(.gender, newValue) }
+    }
 }
