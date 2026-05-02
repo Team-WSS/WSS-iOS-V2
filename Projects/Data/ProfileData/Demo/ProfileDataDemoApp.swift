a73b0068fab7a7d1fc64f62835118e199fc101f4
onesunny2 <ippne996@gmail.com>
2026-05-02 19:12:18 +0900

    [Feat] #71 - ProfileData Demo 타겟 및 앱 추가

---
+++ Projects/Data/ProfileData/Demo/ProfileDataDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileDataDemoApp.swift
+//  ProfileDataDemo
+//
+//  Created by WonsunLee on 5/1/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+import SwiftUI
+import ProfileData
+
+@main
+struct ProfileDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileDataDemoView()
+        }
+    }
+}
