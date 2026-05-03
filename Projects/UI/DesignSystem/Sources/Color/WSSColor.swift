98d862fa6390cf741558bb3973518090ef4059cd
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-05-03 23:28:32 +0900

    [Feat] #15 - WSSColor typealias + extension 추가

---
+++ Projects/UI/DesignSystem/Sources/Color/WSSColor.swift
@@ -0,0 +1,65 @@
+//
+//  WSSColor.swift
+//  DesignSystem
+//
+//  Created by Seoyeon Choi on 5/3/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+import SwiftUI
+
+public typealias WSSColor = DesignSystemAsset.Colors
+
+public extension Color {
+
+    // MARK: - Primary
+
+    static let wssPrimary20 = WSSColor.wssPrimary20.swiftUIColor
+    static let wssPrimary50 = WSSColor.wssPrimary50.swiftUIColor
+    static let wssPrimary100 = WSSColor.wssPrimary100.swiftUIColor
+    static let wssPrimary200 = WSSColor.wssPrimary200.swiftUIColor
+
+    // MARK: - Secondary
+
+    static let wssSecondary100 = WSSColor.wssSecondary100.swiftUIColor
+
+    // MARK: - Gray
+
+    static let wssGray20 = WSSColor.wssGray20.swiftUIColor
+    static let wssGray50 = WSSColor.wssGray50.swiftUIColor
+    static let wssGray70 = WSSColor.wssGray70.swiftUIColor
+    static let wssGray100 = WSSColor.wssGray100.swiftUIColor
+    static let wssGray200 = WSSColor.wssGray200.swiftUIColor
+    static let wssGray300 = WSSColor.wssGray300.swiftUIColor
+    static let wssGrayToast = WSSColor.wssGrayToast.swiftUIColor
+
+    // MARK: - Black & White
+
+    static let wssBlack = WSSColor.wssBlack.swiftUIColor
+    static let wssBlack60 = WSSColor.wssBlack60.swiftUIColor
+    static let wssWhite = WSSColor.wssWhite.swiftUIColor
+
+    // MARK: - Genre Block
+
+    static let blBlock = WSSColor.blBlock.swiftUIColor
+    static let dramaBlock = WSSColor.dramaBlock.swiftUIColor
+    static let fantasyBlock = WSSColor.fantasyBlock.swiftUIColor
+    static let lightBlock = WSSColor.lightBlock.swiftUIColor
+    static let modernBlock = WSSColor.modernBlock.swiftUIColor
+    static let mysteryBlock = WSSColor.mysteryBlock.swiftUIColor
+    static let rofanBlock = WSSColor.rofanBlock.swiftUIColor
+    static let romanceBlock = WSSColor.romanceBlock.swiftUIColor
+    static let wuxiaBlock = WSSColor.wuxiaBlock.swiftUIColor
+
+    // MARK: - Genre Link
+
+    static let blLink = WSSColor.blLink.swiftUIColor
+    static let dramaLink = WSSColor.dramaLink.swiftUIColor
+    static let fantasyLink = WSSColor.fantasyLink.swiftUIColor
+    static let lightLink = WSSColor.lightLink.swiftUIColor
+    static let modernLink = WSSColor.modernLink.swiftUIColor
+    static let mysteryLink = WSSColor.mysteryLink.swiftUIColor
+    static let rofanLink = WSSColor.rofanLink.swiftUIColor
+    static let romanceLink = WSSColor.romanceLink.swiftUIColor
+    static let wuxiaLink = WSSColor.wuxiaLink.swiftUIColor
+}
