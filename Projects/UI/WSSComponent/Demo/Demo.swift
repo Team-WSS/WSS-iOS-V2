9192e98b87126056e44f5ac5b822b10f06c5feb2
onesunny2 <ippne996@gmail.com>
2026-05-10 16:36:27 +0900

    [Feat] #110 - main 진입 점 Demo앱에 추가

--- Projects/UI/WSSComponent/Demo/Demo.swift
+++ Projects/UI/WSSComponent/Demo/Demo.swift
@@ -5,3 +5,20 @@
 //  Created by Seoyeon Choi on 11/25/25.
 //
 
+import SwiftUI
+import WSSComponent
+
+@main
+struct WSSComponentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NetworkErrorView(onRetry: {})
+    }
+}
