d7e0ddd668f70454d994cac86792086ee0d2250c
onesunny2 <ippne996@gmail.com>
2026-05-10 23:22:28 +0900

    [Feat] #109 - SearchKeywordChip 컴포넌트 구현

---
+++ Projects/UI/WSSComponent/Sources/KeywordChips/SearchKeywordChip.swift
@@ -0,0 +1,64 @@
+//
+//  SearchKeywordChip.swift
+//  WSSComponent
+//
+//  Created by WonsunLee on 5/10/26.
+//  Copyright © 2026 kr.websoso.app. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+public struct SearchKeywordChip: View {
+    private let keyword: String
+    private let onTap: () -> Void
+    private let onRemove: () -> Void
+
+    public init(
+        keyword: String,
+        onTap: @escaping () -> Void,
+        onRemove: @escaping () -> Void
+    ) {
+        self.keyword = keyword
+        self.onTap = onTap
+        self.onRemove = onRemove
+    }
+
+    public var body: some View {
+        HStack(spacing: 6) {
+            Text(keyword)
+                .applyWSSFont(.body3, color: .wssPrimary100)
+                .fixedSize()
+
+            Button(action: onRemove) {
+                WSSImage.icKeywordCancel.swiftUIImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 16, height: 16)
+            }
+        }
+        .padding(.horizontal, 13)
+        .padding(.vertical, 5)
+        .background(Color.wssWhite)
+        .clipShape(Capsule())
+        .overlay(Capsule().stroke(Color.wssPrimary100, lineWidth: 1))
+        .contentShape(Capsule())
+        .onTapGesture { onTap() }
+    }
+}
+
+#Preview {
+    VStack(spacing: 12) {
+        SearchKeywordChip(keyword: "환생물", onTap: {
+            print("키워드 클릭")
+        }, onRemove: {
+            print("X 버튼 클릭")
+        })
+        SearchKeywordChip(keyword: "긴 키워드 텍스트 예시", onTap: {
+            print("키워드 클릭")
+        }, onRemove: {
+            print("X 버튼 클릭")
+        })
+    }
+    .padding()
+}
