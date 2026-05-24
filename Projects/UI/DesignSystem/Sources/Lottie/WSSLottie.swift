8ac89c28aea7bcb5ac1fe45cc990d03095bb0ea9
onesunny2 <ippne996@gmail.com>
2026-05-24 18:36:50 +0900

    [Chore] #111 - WSSLottie 목록에서 제거한 에셋 삭제

--- Projects/UI/DesignSystem/Sources/Lottie/WSSLottie.swift
+++ Projects/UI/DesignSystem/Sources/Lottie/WSSLottie.swift
@@ -10,11 +10,5 @@ import Lottie
 
 public enum WSSLottie {
     public static let loading: LottieAnimation?           = .named("loading",           bundle: .module)
-    public static let regressorGreeting: LottieAnimation? = .named("regressorGreeting",  bundle: .module)
-    public static let regressorSword: LottieAnimation?    = .named("regressorSword",     bundle: .module)
     public static let scroll: LottieAnimation?            = .named("scroll",             bundle: .module)
-    public static let sosocatBread: LottieAnimation?      = .named("sosocatBread",       bundle: .module)
-    public static let sosocatTail: LottieAnimation?       = .named("sosocatTail",        bundle: .module)
-    public static let villainessFan: LottieAnimation?     = .named("villainessFan",      bundle: .module)
-    public static let villainessTea: LottieAnimation?     = .named("villainessTea",      bundle: .module)
 }
