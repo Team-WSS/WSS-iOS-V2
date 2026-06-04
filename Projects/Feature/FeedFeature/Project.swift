f33dc40955f165a97154bf586bdd33a56014f13b
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-06-04 19:50:03 +0900

    [Chore] #134 - FeedFeature 모듈 추가

---
+++ Projects/Feature/FeedFeature/Project.swift
@@ -0,0 +1,21 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by Seoyeon Choi on 6/4/26.
+//
+
+import Foundation
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.createFeatureModule(
+    name: ModuleType.feature(.feed).name,
+    targets: [.sources, .demo],
+    internalDependencies: [
+        .module(.domain(.base)),
+        .module(.domain(.feed))
+    ]
+)
