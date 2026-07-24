092ba3fb17cc58766d32e0c3c00b810baa02dbc8
Guryss <lala1470@konkuk.ac.kr>
2026-07-24 19:18:26 +0900

    [Chore] #150 - FeedData 데모 테스트 계정 전환

--- Projects/Data/FeedData/Demo/FeedDataDemoApp.swift
+++ Projects/Data/FeedData/Demo/FeedDataDemoApp.swift
@@ -15,7 +15,7 @@ struct FeedDataDemoApp: App {
 
     init() {
         let storage = UserDefaultsStorage()
-        storage.set(.userID, 10035)
+        storage.set(.userID, 10045)
     }
 
     var body: some Scene {
