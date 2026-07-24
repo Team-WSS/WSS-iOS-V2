1c99b7b478a6faea1e3540671b78f8492c21a9e6
Guryss <lala1470@konkuk.ac.kr>
2026-07-24 19:15:57 +0900

    [Feat] #150 - WSSEmptyType에 내 피드 empty 상태(myFeed) 케이스 추가

--- Projects/UI/WSSComponent/Sources/Empty/WSSEmptyType.swift
+++ Projects/UI/WSSComponent/Sources/Empty/WSSEmptyType.swift
@@ -12,20 +12,23 @@ public enum WSSEmptyType {
     case novel
     case keyword
     case novelNotification
+    case myFeed
     
     var description: String {
         switch self {
-        case .novel:    "해당 검색어를 가진 작품은\n아직 등록되지 않았어요.."
-        case .keyword:  "해당 키워드는\n아직 등록되지 않았어요.."
-        case .novelNotification: "알림 등록한 작품이 없어요"
+        case .novel:                "해당 검색어를 가진 작품은\n아직 등록되지 않았어요.."
+        case .keyword:              "해당 키워드는\n아직 등록되지 않았어요.."
+        case .novelNotification:    "알림 등록한 작품이 없어요"
+        case .myFeed:               "아직 남긴 기록이 없어요"
         }
     }
     
     var buttonTitle: String {
         switch self {
-        case .novel: "작품 문의하러 가기"
-        case .keyword: "키워드 문의하러 가기"
-        case .novelNotification: "작품 둘러보기"
+        case .novel:                "작품 문의하러 가기"
+        case .keyword:              "키워드 문의하러 가기"
+        case .novelNotification:    "작품 둘러보기"
+        case .myFeed:               "글 쓰러 가기"
         }
     }
 }
