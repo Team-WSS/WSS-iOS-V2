2b378904cc6798c19e2f6033afb0311411f82562
Guryss <lala1470@konkuk.ac.kr>
2026-07-27 22:31:02 +0900

    [Design] #172 - MypageView 스크롤뷰 배경을 wssWhite로 설정

--- Projects/Feature/UserPageFeature/Sources/MyPage/MypageView.swift
+++ Projects/Feature/UserPageFeature/Sources/MyPage/MypageView.swift
@@ -83,6 +83,7 @@ struct MypageView: View {
                 }
             }
         }
+        .background(WSSColor.wssWhite.swiftUIColor)
         .toolbar {
             createMypageViewToolBarContent()
         }
